6adf033772505c50318e4da30a26d93667d0afc3
Mariano Villarreal <marianov994@gmail.com>
2025-11-17 18:30:20 -0300

    remove old components

--- src/components/admin/index.js
+++
@@ -1,4 +0,0 @@
-export { QuickAction } from './QuickAction';
-export { FilterButton } from './FilterButton';
-export { UserListItem } from './UserListItem';
-export { Pagination } from './Pagination';
--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const LOCAL_STORAGE_KEYS = Object.freeze({
-  USER: 'user',
-  CART: 'cart',
-});
-
-export const TAX_RATE = 10.5;
-
-export const DEFAULT_API_URL = 'http://localhost:8000';
